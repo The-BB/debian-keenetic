d1d8e79b64b447a7eae40d952c870159a1a5b984
The-BB <tun.chen.bo@gmail.com>
2019-04-29 14:35:56 +0300

    debian-stretch-mips.sh: updated Debian 9: 9.9 released

--- debian-stretch-mips.sh
+++ debian-stretch-mips.sh
@@ -5,16 +5,25 @@ SCRIPT_DIR=$(dirname $0)
 
 ROOT_DIR=$SCRIPT_DIR/installer_root
 BUILD_DIR=$SCRIPT_DIR/../Entware/build_dir/target-mips_mips32r2_glibc-*
-INSTALLER=$SCRIPT_DIR/debian-stretch-9_8-mips.tar.gz
+INSTALLER=$SCRIPT_DIR/debian-stretch-9_9-mips.tar.gz
 
 # Compile libc and busybox from Entware first!
 [ -d $BUILD_DIR ] || exit 1
 
 [ -d $ROOT_DIR ] && rm -fr $ROOT_DIR
 # Make hook scripts dirs, see https://github.com/ndmsystems/packages/wiki/Opkg-Component
-for dir in button fs netfilter schedule time usb user wan; do
-    mkdir -p $ROOT_DIR/opt/etc/ndm/${dir}.d
-done
+#for dir in button fs neighbour netfilter schedule time usb user wan \
+#	ifcreated ifdestroyed ifipchanged ifstatechanged \
+#	l2tp_ipsec_vpn_up l2tp_ipsec_vpn_down pptp_vpn_up pptp_vpn_down	\
+#	sstp_vpn_up sstp_vpn_down vip_vpn_up vip_vpn_down \
+#	openvpn-up openvpn-down openvpn-route-up openvpn-ipchange
+#	openvpn-client-connect openvpn-client-disconnect \
+#	openvpn-learn-address openvpn-tls-verify; do
+#    mkdir -p $ROOT_DIR/opt/etc/ndm/${dir}.d
+#done
+echo 'Adding hook scripts dirs'
+mkdir -p $ROOT_DIR/opt/etc
+cp -r $BUILD_DIR/opt-ndmsv2-*/ipkg-mips-3.4_kn/opt-ndmsv2/opt/etc/ndm $ROOT_DIR/opt/etc/
 
 echo 'Adding toolchain libraries...'
 cp -r $BUILD_DIR/toolchain/ipkg-mips-3.4/libc/opt $ROOT_DIR
@@ -25,7 +34,7 @@ echo 'Adding busybox...'
 cp -r $BUILD_DIR/busybox-*/ipkg-install/opt $ROOT_DIR
 
 echo 'Adding iptables...'
-cp -r $SCRIPT_DIR/../Entware/build_dir/target-mips_mips32r2_glibc-*/linux-mips-3.4/iptables-*/ipkg-mips-3.4/iptables/opt $ROOT_DIR
+cp -r $BUILD_DIR/linux-mips-3.4/iptables-*/ipkg-mips-3.4/iptables/opt $ROOT_DIR
 
 echo 'Adding Debian minimal...'
 [ -f debian-stretch-mips_clean.tgz ] || wget http://ndm.zyxmon.org/binaries/debian/debian-stretch-mips_clean.tgz
@@ -52,6 +61,7 @@ sudo echo 'nameserver 8.8.8.8' > $ROOT_DIR/opt/debian/etc/resolv.conf
 echo 'Adding start script...'
 mkdir -p $ROOT_DIR/opt/etc
 cp $SCRIPT_DIR/initrc $ROOT_DIR/opt/etc
+chmod +x $ROOT_DIR/opt/etc/initrc
 
 echo 'Adding ndmq utility...'
 sudo tar -xz -C $ROOT_DIR/opt/debian -f ndmq-mips.tgz

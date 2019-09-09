f6b3e12a43b09ed0d8998c055bd5b0637a94c8e2
The-BB <tun.chen.bo@gmail.com>
2019-09-09 16:21:57 +0300

    debian-stretch-mipsel.sh: (hotfix) updated Debian 9: 9.11 released

--- debian-stretch-mipsel.sh
+++ debian-stretch-mipsel.sh
@@ -5,7 +5,7 @@ SCRIPT_DIR=$(dirname $0)
 
 ROOT_DIR=$SCRIPT_DIR/installer_root
 BUILD_DIR=$SCRIPT_DIR/../Entware/build_dir/target-mipsel_mips32r2_glibc-*
-INSTALLER=$SCRIPT_DIR/debian-stretch-9_10-mipsel.tar.gz
+INSTALLER=$SCRIPT_DIR/debian-stretch-9_11-mipsel.tar.gz
 
 # Compile libc and busybox from Entware first!
 [ -d $BUILD_DIR ] || exit 1
